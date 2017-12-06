ead5c69e28976bf1aaf6494d1ff4e2ad116b49aa
Ekaterina Svetlakova <radistkatya@gmail.com>
2017-12-06 17:35:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var recipes = {
 };
 function updateObjectWithKeyAndValue(object, key, value) {
   var object = { key: 'value' }
-  Object.assign( object, {key: 'value'})
+  object = Object.assign( object, {key: 'value'})
   return object
 }
