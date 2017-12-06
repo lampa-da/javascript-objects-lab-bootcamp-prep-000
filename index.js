7f77460e5870cdecce2e0fae5fafc1a51f21aaac
Ekaterina Svetlakova <radistkatya@gmail.com>
2017-12-06 18:08:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object, key) {
-  delete object.key
+delete Object.assign({}, object).key
   return object
 }
